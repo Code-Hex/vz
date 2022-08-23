56bd66ee92765b1492c4461d6232a39a4094c212
Kei Kamikawa <code-hex@users.noreply.github.com>
2022-08-24 02:05:42 +0900

    added about panel and some menu items

--- virtualization_view.h
+++ virtualization_view.h
@@ -10,15 +10,13 @@
 #import <Cocoa/Cocoa.h>
 #import <Virtualization/Virtualization.h>
 
-// @interface CanBecomeKeyWindow : NSWindow
-// @property(readonly) BOOL canBecomeKeyWindow;
-// @end
+@interface AboutViewController : NSViewController
+- (instancetype)init;
+@end
 
-// @interface GraphicsAppTitlebarViewController : NSTitlebarAccessoryViewController
-// - (instancetype)initWithVirtualMachine:(VZVirtualMachine *)virtualMachine
-//     windowWidth:(CGFloat)windowWidth
-//     windowHeight:(CGFloat)windowHeight;
-// @end
+@interface AboutPanel : NSPanel
+- (instancetype)init;
+@end
 
 @interface AppDelegate : NSObject <NSApplicationDelegate, NSWindowDelegate, VZVirtualMachineDelegate>
 - (instancetype)initWithVirtualMachine:(VZVirtualMachine *)virtualMachine
--- virtualization_view.m
+++ virtualization_view.m
@@ -6,6 +6,122 @@
 
 #import "virtualization_view.h"
 
+@implementation AboutViewController {}
+
+- (instancetype)init
+{
+    self = [super initWithNibName:nil bundle:nil];
+    return self;
+}
+
+- (void)loadView
+{
+    self.view = [NSView new];
+    NSImageView *imageView = [NSImageView imageViewWithImage:[NSApp applicationIconImage]];
+    NSTextField *appLabel = [self makeLabel:[[NSProcessInfo processInfo] processName]];
+    [appLabel setFont:[NSFont boldSystemFontOfSize:16]];
+    NSTextField *subLabel = [self makePoweredByLabel];
+
+    NSStackView *stackView = [NSStackView stackViewWithViews:@[
+        imageView,
+        appLabel,
+        subLabel,
+    ]];
+    [stackView setOrientation:NSUserInterfaceLayoutOrientationVertical];
+    [stackView setDistribution:NSStackViewDistributionFillProportionally];
+    [stackView setSpacing:10];
+    [stackView setAlignment:NSLayoutAttributeCenterX];
+    [stackView setContentCompressionResistancePriority:NSLayoutPriorityRequired forOrientation:NSLayoutConstraintOrientationHorizontal];
+    [stackView setContentCompressionResistancePriority:NSLayoutPriorityRequired forOrientation:NSLayoutConstraintOrientationVertical];
+
+    [self.view addSubview:stackView];
+
+    [NSLayoutConstraint activateConstraints:@[
+        [imageView.widthAnchor constraintEqualToConstant:80], // image size
+        [imageView.heightAnchor constraintEqualToConstant:80], // image size
+        [stackView.topAnchor constraintEqualToAnchor:self.view.topAnchor constant:4],
+        [stackView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor constant:-16],
+        [stackView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:32],
+        [stackView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:-32],
+        [stackView.widthAnchor constraintEqualToConstant:300]
+    ]];
+}
+
+- (NSTextField *)makePoweredByLabel
+{
+    NSMutableAttributedString *poweredByAttr = [[[NSMutableAttributedString alloc]
+        initWithString:@"Powered by " attributes:@{
+            NSForegroundColorAttributeName: [NSColor labelColor]
+        }] autorelease];
+    NSURL *repositoryURL = [NSURL URLWithString:@"https://github.com/Code-Hex/vz"];
+    NSMutableAttributedString *repository = [self makeHyperLink:@"github.com/Code-Hex/vz" withURL:repositoryURL];
+    [poweredByAttr appendAttributedString:repository];
+    [poweredByAttr addAttribute:NSFontAttributeName
+        value:[NSFont systemFontOfSize:12]
+        range:NSMakeRange(0, [poweredByAttr length])];
+
+    NSTextField *label = [self makeLabel:@""];
+    [label setSelectable:YES];
+    [label setAllowsEditingTextAttributes:YES];
+    [label setAttributedStringValue:poweredByAttr];
+    return label;
+}
+
+- (NSTextField *)makeLabel:(NSString *)label
+{
+    NSTextField *appLabel = [NSTextField labelWithString:label];
+    [appLabel setTextColor:[NSColor labelColor]];
+    [appLabel setEditable:NO];
+    [appLabel setSelectable:NO];
+    [appLabel setBezeled:NO];
+    [appLabel setBordered:NO];
+    [appLabel setBackgroundColor:[NSColor clearColor]];
+    [appLabel setAlignment:NSTextAlignmentCenter];
+    [appLabel setLineBreakMode:NSLineBreakByWordWrapping];
+    [appLabel setUsesSingleLineMode:NO];
+    [appLabel setMaximumNumberOfLines:20];
+    return appLabel;
+}
+
+// https://developer.apple.com/library/archive/qa/qa1487/_index.html
+- (NSMutableAttributedString *)makeHyperLink:(NSString *)inString withURL:(NSURL*)aURL
+{
+    NSMutableAttributedString* attrString = [[NSMutableAttributedString alloc] initWithString:inString];
+    NSRange range = NSMakeRange(0, [attrString length]);
+
+    [attrString beginEditing];
+    [attrString addAttribute:NSLinkAttributeName value:[aURL absoluteString] range:range];
+
+    // make the text appear in blue
+    [attrString addAttribute:NSForegroundColorAttributeName value:[NSColor blueColor] range:range];
+
+    // next make the text appear with an underline
+    [attrString addAttribute:NSUnderlineStyleAttributeName
+        value:[NSNumber numberWithInt:NSUnderlineStyleSingle] range:range];
+
+    [attrString endEditing];
+    return [attrString autorelease];
+}
+
+@end
+
+@implementation AboutPanel {}
+
+- (instancetype)init
+{
+    self = [super initWithContentRect:NSZeroRect styleMask:NSWindowStyleMaskTitled|NSWindowStyleMaskClosable backing:NSBackingStoreBuffered defer:NO];
+
+    AboutViewController *viewController = [[[AboutViewController alloc] init] autorelease];
+    [self setContentViewController:viewController];
+    [self setTitleVisibility:NSWindowTitleHidden];
+    [self setTitlebarAppearsTransparent:YES];
+    [self setBecomesKeyOnlyIfNeeded:NO];
+    [self center];
+    return self;
+}
+
+@end
+
 @implementation AppDelegate {
     VZVirtualMachine *_virtualMachine;
     VZVirtualMachineView *_virtualMachineView;
@@ -23,7 +139,7 @@
 
     // Setup virtual machine view configs
     VZVirtualMachineView *view = [[[VZVirtualMachineView alloc] init] autorelease];
-    view.capturesSystemKeys = NO;
+    view.capturesSystemKeys = YES;
     view.virtualMachine = _virtualMachine;
     _virtualMachineView = view;
 
@@ -69,7 +185,6 @@
     [window setOpaque:NO];
     [window setContentView:_virtualMachineView];
     // [window setDelegate:self];
-    // [window setInitialFirstResponder:view];
     [window setTitleVisibility:NSWindowTitleHidden];
     [window center];
 
@@ -92,6 +207,12 @@
     NSMenuItem *windowMenuItem = [[[NSMenuItem alloc] initWithTitle:@"Window" action:nil keyEquivalent:@""] autorelease];
     [menuBar addItem:windowMenuItem];
     [windowMenuItem setSubmenu:windowMenu];
+
+    // Help menu
+    NSMenu *helpMenu = [self setupHelpMenu];
+    NSMenuItem *helpMenuItem = [[[NSMenuItem alloc] initWithTitle:@"Help" action:nil keyEquivalent:@""] autorelease];
+    [menuBar addItem:helpMenuItem];
+    [helpMenuItem setSubmenu:helpMenu];
 }
 
 
@@ -99,6 +220,18 @@
 {
     NSMenu *appMenu = [[[NSMenu alloc] init] autorelease];
     NSString *applicationName = [[NSProcessInfo processInfo] processName];
+
+    NSMenuItem *aboutMenuItem = [[[NSMenuItem alloc]
+        initWithTitle:[NSString stringWithFormat:@"About %@", applicationName]
+        action:@selector(openAboutWindow:)
+        keyEquivalent:@""] autorelease];
+
+    // CapturesSystemKeys toggle
+    NSMenuItem *capturesSystemKeysItem = [[[NSMenuItem alloc]
+        initWithTitle:@"Enable to send system hot keys to virtual machine"
+        action:@selector(toggleCapturesSystemKeys:)
+        keyEquivalent:@""] autorelease];
+    [capturesSystemKeysItem setState:[self capturesSystemKeysState]];
 
     // Service menu
     NSMenuItem *servicesMenuItem = [[[NSMenuItem alloc] initWithTitle:@"Services" action:nil keyEquivalent:@""] autorelease];
@@ -113,6 +246,10 @@
     [hideOthersItem setKeyEquivalentModifierMask:(NSEventModifierFlagOption|NSEventModifierFlagCommand)];
 
     NSArray *menuItems = @[
+        aboutMenuItem,
+        [NSMenuItem separatorItem],
+        capturesSystemKeysItem,
+        [NSMenuItem separatorItem],
         servicesMenuItem,
         [NSMenuItem separatorItem],
         [[[NSMenuItem alloc]
@@ -138,9 +275,8 @@
     NSArray *menuItems = @[
         [[[NSMenuItem alloc] initWithTitle:@"Minimize" action:@selector(performMiniaturize:) keyEquivalent:@"m"] autorelease],
         [[[NSMenuItem alloc] initWithTitle:@"Zoom" action:@selector(performZoom:) keyEquivalent:@""] autorelease],
-        [[[NSMenuItem alloc] initWithTitle:@"Close Window" action:@selector(performClose:) keyEquivalent:@"w"] autorelease],
-        [[[NSMenuItem alloc] initWithTitle:@"Copy" action:@selector(copy:) keyEquivalent:@"c"] autorelease],
-        [[[NSMenuItem alloc] initWithTitle:@"Paste" action:@selector(paste:) keyEquivalent:@"v"] autorelease],
+        [NSMenuItem separatorItem],
+        [[[NSMenuItem alloc] initWithTitle:@"Bring All to Front" action:@selector(arrangeInFront:) keyEquivalent:@""] autorelease],
     ];
     for (NSMenuItem *menuItem in menuItems) {
         [windowMenu addItem:menuItem];
@@ -149,4 +285,40 @@
     return windowMenu;
 }
 
+- (NSMenu *)setupHelpMenu
+{
+    NSMenu *helpMenu = [[[NSMenu alloc] initWithTitle:@"Help"] autorelease];
+    NSArray *menuItems = @[
+        [[[NSMenuItem alloc] initWithTitle:@"Report issue" action:@selector(reportIssue:) keyEquivalent:@""] autorelease],
+    ];
+    for (NSMenuItem *menuItem in menuItems) {
+        [helpMenu addItem:menuItem];
+    }
+    [NSApp setHelpMenu:helpMenu];
+    return helpMenu;
+}
+
+- (void)toggleCapturesSystemKeys:(id)sender
+{
+    NSMenuItem *item = (NSMenuItem*)sender;
+    _virtualMachineView.capturesSystemKeys = !_virtualMachineView.capturesSystemKeys;
+    [item setState:[self capturesSystemKeysState]];
+}
+
+- (NSControlStateValue)capturesSystemKeysState
+{
+    return _virtualMachineView.capturesSystemKeys ? NSControlStateValueOn : NSControlStateValueOff;
+}
+
+- (void)reportIssue:(id)sender
+{
+    NSString *url = @"https://github.com/Code-Hex/vz/issues/new";
+    [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:url]];
+}
+
+- (void)openAboutWindow:(id)sender
+{
+    AboutPanel *aboutPanel = [[[AboutPanel alloc] init] autorelease];
+    [aboutPanel makeKeyAndOrderFront:nil];
+}
 @end

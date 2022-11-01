62e1314581fc11fd861ca06b1496f53498a65ed8
Kei Kamikawa <code-hex@users.noreply.github.com>
2022-11-01 16:37:33 +0900

    fmt

--- virtualization_helper.h
+++ virtualization_helper.h
@@ -24,7 +24,8 @@ NSDictionary *dumpProcessinfo();
 #pragma message("macOS 13 API has been disabled")
 #endif
 
-static inline int mac_os_x_version_max_allowed() {
+static inline int mac_os_x_version_max_allowed()
+{
 #ifdef __MAC_OS_X_VERSION_MAX_ALLOWED
     return __MAC_OS_X_VERSION_MAX_ALLOWED;
 #else

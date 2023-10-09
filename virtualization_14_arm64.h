b3e990ef1712dbb6247563310e0fa84f0cae2cc7
Kei Kamikawa <Code-Hex@users.noreply.github.com>
2023-10-09 21:56:09 +0900

    dirty hack for clang-format

--- virtualization_14_arm64.h
+++ virtualization_14_arm64.h
@@ -3,13 +3,17 @@
 //
 //  Created by codehex.
 //
-
 #pragma once
+
+#ifdef __arm64__
+
+// FIXME(codehex): this is dirty hack to avoid clang-format error like below
+// "Configuration file(s) do(es) not support C++: /github.com/Code-Hex/vz/.clang-format"
+#define NSURLComponents NSURLComponents
 
 #import "virtualization_helper.h"
 #import <Virtualization/Virtualization.h>
 
-#ifdef __arm64__
 void saveMachineStateToURLWithCompletionHandler(void *machine, void *queue, uintptr_t cgoHandle, const char *saveFilePath);
 void restoreMachineStateFromURLWithCompletionHandler(void *machine, void *queue, uintptr_t cgoHandle, const char *saveFilePath);
 void *newVZLinuxRosettaAbstractSocketCachingOptionsWithName(const char *name, void **error);

d4e78b9ccc095b3766279802f869b3d6d4d79bab
Kei Kamikawa <Code-Hex@users.noreply.github.com>
2023-10-22 18:22:29 +0900

    added comment for ValidateSaveRestoreSupport

--- configuration_arm64.go
+++ configuration_arm64.go
@@ -11,6 +11,13 @@ package vz
 import "C"
 import "github.com/Code-Hex/vz/v3/internal/objc"
 
+// ValidateSaveRestoreSupport Determines whether the framework can save or restore the VM’s current configuration.
+//
+// Verify that a virtual machine with this configuration is savable.
+// Not all configuration options can be safely saved and restored from file.
+//
+// If this evaluates to false, the caller should expect future calls to `(*VirtualMachine).SaveMachineStateToPath` to fail.
+// error If not nil, assigned with an error describing the unsupported configuration option.
 func (v *VirtualMachineConfiguration) ValidateSaveRestoreSupport() (bool, error) {
 	nserrPtr := newNSErrorAsNil()
 	ret := C.validateSaveRestoreSupportWithError(objc.Ptr(v), &nserrPtr)

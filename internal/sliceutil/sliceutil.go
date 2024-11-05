4be99b6e2aa560b6ca2f1c439439c1ad15eddea8
Kei Kamikawa <Code-Hex@users.noreply.github.com>
2024-11-05 09:17:35 +0900

    fixed FindValueByIndex logic

--- internal/sliceutil/sliceutil.go
+++ internal/sliceutil/sliceutil.go
@@ -1,12 +1,10 @@
 package sliceutil
 
 // FindValueByIndex returns the value of the index in s,
-// or -1 if not present.
+// or zero value if not present.
 func FindValueByIndex[S ~[]E, E any](s S, idx int) (v E) {
-	for i := range s {
-		if i == idx {
-			return s[i]
-		}
+	if idx < 0 || idx >= len(s) {
+		return v // return zero value of type E
 	}
-	return v
+	return s[idx]
 }

e669237d0ee813976a729143769c1ed35d05ae05
Peter Hasko-Nagy <peter@depot.dev>
2025-08-28 12:49:08 +0200

    feat: add ASIF support (macOS 26.0)

--- disk.go
+++ disk.go
@@ -1,7 +1,11 @@
 package vz
 
 import (
+	"context"
+	"fmt"
 	"os"
+	"os/exec"
+	"strconv"
 )
 
 // CreateDiskImage is creating disk image with specified filename and filesize.
@@ -19,5 +23,39 @@ func CreateDiskImage(pathname string, size int64) error {
 	if err := f.Truncate(size); err != nil {
 		return err
 	}
+	return nil
+}
+
+// CreateSparseDiskImage is creating an "Apple Sparse Image Format" disk image
+// with specified filename and filesize. The function "shells out" to diskutil, as currently
+// this is the only known way of creating ASIF images.
+// For example, if you want to create disk with 64GiB, you can set "64 * 1024 * 1024 * 1024" to size.
+//
+// Note that ASIF is only available from macOS Tahoe, so the function will return error
+// on earlier versions.
+func CreateSparseDiskImage(ctx context.Context, pathname string, size int64) error {
+	if err := macOSAvailable(26); err != nil {
+		return err
+	}
+	diskutil, err := exec.LookPath("diskutil")
+	if err != nil {
+		return fmt.Errorf("failed to find disktuil: %w", err)
+	}
+
+	sizeStr := strconv.FormatInt(size, 10)
+	cmd := exec.CommandContext(ctx,
+		diskutil,
+		"image",
+		"create",
+		"blank",
+		"--fs", "none",
+		"--format", "ASIF",
+		"--size", sizeStr,
+		pathname)
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to create ASIF disk image: %w", err)
+	}
+
 	return nil
 }
